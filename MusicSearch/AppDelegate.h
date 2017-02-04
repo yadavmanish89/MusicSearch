42182486695308ab406f2a3d0afd63656a8ed160
yadavmanish89 <yadavmanish89@gmail.com>
2017-02-04 13:15:28 -0500

    Inital Commit

---
+++ MusicSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MusicSearch
+//
+//  Created by Yadav, Manish on 2/4/17.
+//  Copyright © 2017 Yadav, Manish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MusicSearch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicSearch
+//
+//  Created by Yadav, Manish on 2/4/17.
+//  Copyright © 2017 Yadav, Manish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
